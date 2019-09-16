22a98f0aa668dae5172beb6cd943a8f7b3fd4bc1
Érika Assis <erika.s.assis@gmail.com>
2019-09-16 12:47:47 -0300

    Alterada configuração da porta do servidor

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
 
-app.listen(3000);
+app.listen(PORT);
